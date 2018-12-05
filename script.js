003277e8b6ebcc144f9457790ffedfbf85776c94
dahboss510 <42359351+dahboss510@users.noreply.github.com>
2018-12-05 14:58:46 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#button2").click(function() {
+    $("#y").fadeToggle();
+});
+$("#button1").click(function() {
+    $("#not").slideToggle();
+});
+$("#button3").click(function() {
+    $("#hello").toggle();
+});
